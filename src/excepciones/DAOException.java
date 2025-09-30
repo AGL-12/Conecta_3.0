aeb224dddb222b8617da6798149d5e38714cee57
AGL-12” <aglgg21@gmail.com>
2025-09-30 14:07:37 +0200

    Conecta 3.0

---
+++ src/excepciones/DAOException.java
@@ -0,0 +1,17 @@
+
+package excepciones;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class DAOException extends Exception {
+
+    public DAOException(String message) {
+        super(message);
+    }
+
+    public DAOException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
